fdbc54a22e2bacc920e8e1001f8368623055d285
Silke Horn <silke@solros.de>
2014-10-05 22:10:05 +0200

    implemented default behavior: open webbrowser

--- apps/common/perllib/ThreeJS.pm
+++ apps/common/perllib/ThreeJS.pm
@@ -68,15 +68,16 @@ $title
 <script src="js/Detector.js"></script>
 
 <script>
+	var renderer = Detector.webgl? new THREE.WebGLRenderer(): new THREE.CanvasRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+
+
 	var scene = new THREE.Scene();
 	var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 	
 	var controls = new THREE.TrackballControls( camera );
 				
-	var renderer = Detector.webgl? new THREE.WebGLRenderer(): new THREE.CanvasRenderer();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	document.body.appendChild(renderer.domElement);
-
 	camera.position.z = 5;		
 %
 }
